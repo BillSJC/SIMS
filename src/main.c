eb332a2d46653990e0216cadae28ed003f71a9dc
SJC <707425690@qq.com>
2018-10-25 10:57:15 +0800

    init list

--- src/main.c
+++ src/main.c
@@ -33,7 +33,7 @@ typedef struct node Node;
         the node struct of list
     */
 struct node{
-   Student* data;
+   Student data;
    //next node,end with NULL
    Node*    next;
    //last node,start with NULL
@@ -43,18 +43,15 @@ struct node{
 /* initStudent
     declear a new student student and give it value
 */
-Student* initStudent(char* stuno,char* name,int sex,char* phone,float scoreChinese,float scoreMath){
-    Student* stu = (Student*)malloc(sizeof(Student));
+Student initStudent(char* stuno,char* name,int sex,char* phone,float scoreChinese,float scoreMath){
+    Student stu;
     //if memory not enogh then return NULL
-    if(stu == NULL){
-        return stu;
-    }
-    stu->stuno          = stuno;
-    stu->name           = name;
-    stu->phone          = phone;
-    stu->sex            = sex;
-    stu->scoreChinese   = scoreChinese;
-    stu->scoreMath      = scoreMath;
+    stu.stuno          = stuno;
+    stu.name           = name;
+    stu.phone          = phone;
+    stu.sex            = sex;
+    stu.scoreChinese   = scoreChinese;
+    stu.scoreMath      = scoreMath;
     return stu;
 }
 
@@ -68,25 +65,15 @@ Node* initEmptyList(){
 /* insertNodeAtNext
     insert a new node after current node and give it data
 */
-int insertNodeAtNext(Node* n,Student* data){
-    Node* temp = NULL;
-    //if this node is not the end of list then store it
-    if(n->next != NULL){
-        temp = n->next;
+int insertNodeAtEnd(Node* pn,Student data){
+    //Node* pn = n;
+    while(pn->next == NULL){
+        pn = pn->next;
     }
-    n->next = (Node*)malloc(sizeof(Node));
-    //if memory not enough then return 0
-    if(n->next == NULL){
-        return 0;
-    }
-    n->next->data = initStudent(data->stuno,data->name,data->sex,data->phone,data->scoreChinese,data->scoreMath);
-    n->next->last = n;
-    //if the temp isn`t null then set it after n->next
-    if(temp != NULL){
-        temp->last = n->next;
-        n->next->next = temp;
-    }
-    return 1;
+    pn->next = (Node*)malloc(sizeof(Node));
+    pn = pn->next;
+    pn->next = NULL;
+    pn->data = data;
 }
 
 
@@ -95,5 +82,8 @@ int insertNodeAtNext(Node* n,Student* data){
     the main function
 */
 int main(){
-    Node* listHeaed ;
+    Node* stuList = (Node*)malloc(sizeof(Node));
+    insertNodeAtEnd(stuList,initStudent("17051823","SJC",1,"18258462816",80.0,85.0));
+    printf("%s\n",stuList->next->data.name);
+    system("pause");
 }
