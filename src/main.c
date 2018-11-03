ceab754ca6a134810be7727b82b9be71a68f87e7
SJC <sjc@debug.online>
2018-11-03 17:47:52 +0800

    more func

--- src/main.c
+++ src/main.c
@@ -4,12 +4,13 @@ last edit 2018.10.20
 for class 数据结构 only
 */
 
-#define FILEPATH "data.csv"
+#define FILEPATH "D:\\数据结构\\第一次大作业\\src\\data.csv"
 #define STRING_MAXLEN 200
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<conio.h>
 
     /* student
         the struct of student infomation
@@ -51,9 +52,15 @@ struct node{
 Student initStudent(char* stuno,char* name,int sex,char* phone,float scoreChinese,float scoreMath){
     Student stu;
     //if memory not enogh then return NULL
-    stu.stuno          = stuno;
-    stu.name           = name;
-    stu.phone          = phone;
+    char* stunoc = (char*)malloc(sizeof(char)*STRING_MAXLEN);
+    strcpy(stunoc,stuno);
+    char* namec = (char*)malloc(sizeof(char)*STRING_MAXLEN);
+    strcpy(namec,name);
+    char* phonec = (char*)malloc(sizeof(char)*STRING_MAXLEN);
+    strcpy(phonec,phone);
+    stu.stuno          = stunoc;
+    stu.name           = namec;
+    stu.phone          = phonec;
     stu.sex            = sex;
     stu.scoreChinese   = scoreChinese;
     stu.scoreMath      = scoreMath;
@@ -80,34 +87,28 @@ Node* initNode(Student st){
 /* insertNodeAtEnd
     insert a new node after current node and give it data
 */
-int insertNodeAtEnd(Head* head,Student data){
-    Node* pnp;
-    pnp = initNode(data);
+Head* insertNodeAtEnd(Head* head,Student data){
     Node* pn;
     pn = head->next;
     if(pn == NULL){
-        head->next = pnp;
-        head->next->last = NULL;
-        return 2;
+        head->next = initNode(data);
+        return head;
     }
     while(pn->next != NULL){
         pn = pn->next;
     }
-    pn->next = pnp;
-    pn->next->last = pn;
-    return 1;
+    pn->next = initNode(data);
+    return head;
 }
 
 char*  strReplace(char* str){
     int i = 0;
-    printf("RAW:%s\n",str);
     while(str[i]!='\0'){
         if(str[i]==','){
             str[i] = ' ';
         }
         i++;
     }
-    printf("NOW:%s\n",str);
     return str;
 }
 
@@ -124,33 +125,179 @@ Head* readFromCSV(){
     float scoreM = 0.0;
     Head* head = initEmptyList();
     FILE* fp ;
-    fp = fopen("D:\\数据结构\\第一次大作业\\src\\data.csv","r");
+    fp = fopen(FILEPATH,"r");
     if(fp == NULL){
-        printf("file poninter error,system will exit\n");
-        system("pause");
-        exit(0);
-        
+        head->next = NULL;
+        return head;
     }
     while(!feof(fp)){
         fscanf(fp,"%s\n",temp);
         temp = strReplace(temp);
         sscanf(temp,"%s %s %s %d %lf %lf",stuno,name,phone,&sex,&scoreC,&scoreM);
-        insertNodeAtEnd(head,initStudent(stuno,name,sex,phone,scoreC,scoreM));
+        head = insertNodeAtEnd(head,initStudent(stuno,name,sex,phone,scoreC,scoreM));
     }
+    fclose(fp);
     return head;
+}
+
+char* sexTransfer(int input){
+    if(input){
+        return "男";
+    }else{
+        return "女";
+    }
+}
+
+void showStudentList(Head* head){
+    int id = 0;
+    printf("%6s|%15s|%15s|%15s|%5s|%10s|%10s\n","id","学号","姓名","手机","性别","语文","数学");
+    Node* np = head->next;
+    while(np != NULL){
+        id++;
+        printf("----------------------------------------------------------------------------\n");
+        printf("%6d|%15s|%15s|%15s|%5s|%10.2lf|%10.2lf\n",id,np->data.stuno,np->data.name,np->data.phone,sexTransfer(np->data.sex),np->data.scoreChinese,np->data.scoreMath);
+        np = np->next;
+    }
+    char temp = getch();
+}
+
+void deleteStudent(Head* head){
+    system("cls");
+    printf("\n==================== 删除学生 ====================\n\n"
+    "              请输入要删除的id,并按回车键"
+    "\n"
+    "==================================================\n\n\n");
+    showStudentList(head);
+    int delid = 0;
+    int i=0;
+    int flag = 0;
+    scanf("%d",&delid);
+    Node* np = head->next;
+    i = 1;
+    while(np != NULL){
+        if(i==delid){
+            
+        }
+    }
+}
+
+void addStudentMenu(Head* head){
+    system("cls");
+    printf("\n==================== 添加学生 ====================\n\n"
+    "              请按照以下格式输入要添加的学生信息"
+    "\n"
+    "        学号,姓名,手机号,性别,语文成绩,数学成绩\n"
+    "\n             输入完成后请按回车键，输入#返回上级菜单\n"
+    "\n"
+    "==================================================\n\n\n");
+    char* stuno = (char*)malloc(sizeof(char)*STRING_MAXLEN);
+    char* name = (char*)malloc(sizeof(char)*STRING_MAXLEN);
+    char* phone = (char*)malloc(sizeof(char)*STRING_MAXLEN);
+    int sex = 0;
+    int temp = 0;
+    char* temps = (char*)malloc(sizeof(char)*STRING_MAXLEN*5);
+    float chinese = 0.0;
+    float math = 0.0;
+    while(1){
+        scanf("%s",temps);
+        if(strcmp(temps,"#")==0){
+            return;
+        }
+        temps = strReplace(temps);
+        temp = sscanf(temps,"%s %s %s %d %f %f",stuno,name,phone,&sex,&chinese,&math);\
+        if(temp == 6){
+            break;
+        }
+    }
+    insertNodeAtEnd(head,initStudent(stuno,name,sex,phone,chinese,math));
+    system("cls");
+    printf("\n==================== 添加学生 ====================\n\n"
+    "                   添加成功，按任意键返回"
+    "\n"
+    "==================================================\n\n\n");
+    getch();
+    return;
+}
+
+void showAllStuList(Head* head){
+    while(1){
+        system("cls");
+        printf("\n==================== 学生列表 ====================\n\n"
+        "                     1、列表排序\n"
+        "                     2、修改信息\n"
+        "                     3、添加学生\n"
+        "                     4、删除学生\n\n"
+        "                     0、返回\n"
+        "==================================================\n\n\n");
+        showStudentList(head);
+        char temp = (char)getch();
+        switch(temp){
+            case '0':{
+                system("cls");
+                return;
+            }
+            case '1':{
+                break;
+            }
+            case '2':{
+                break;
+            }
+            case '3':{
+                addStudentMenu(head);
+                break;
+            }
+            case '4':{
+                break;
+            }
+        }
+    }
+    
+
+}
+
+void mainMenu(Head* head){
+    char input;
+    int temp;
+    while(1){
+    system("cls");
+    printf("==================== 学生信息管理系统 ====================\n"
+"\n"
+"                     1、学生列表\n"
+"                     2、学生查询\n"
+"                     3、添加学生\n"
+"                     \n"
+"                     0、退出系统\n");
+
+    input = (char)getch();
+    switch(input){
+        case '1':{
+            showAllStuList(head);
+            break;
+        };
+        case '2':{
+            break;
+        }
+        case '3':{
+            addStudentMenu(head);
+            break;
+        }
+        case '0':{
+            break;
+        }
+        default:{
+            break;
+        }
+    }
+    
+    }
 }
 
 /* main
     the main function
 */
 int main(){
-    system("chcp 65001"); //set output charset as GBK
+    //system("chcp 65001"); //set output charset as GBK
     Head* stuList = readFromCSV();
-    int i = 0;
-    Node* np = stuList->next;
-    if(np != NULL){
-        printf("%d:%s\n",i,np->data.name);
-        np = np->next;
-    }
+    mainMenu(stuList);
     system("pause");
 }
